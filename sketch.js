cb4efaa7fdb4408a5236a8215adca0f1eef2dab2
High-Flying-Fox <161777532+High-Flying-Fox@users.noreply.github.com>
2026-06-16 23:12:44 -0300

    asjdbajgbajbawdj

--- sketch.js
+++ sketch.js
@@ -52,18 +52,6 @@ function preload() {
 // setup
 function setup() {
 
-  //i hate mobile
-  document.addEventListener('touchmove', function(e) {
-    // Prevent default only if at the top of the page and swiping down
-    if (window.scrollY === 0 && e.touches[0].pageY > 0) {
-        e.preventDefault();
-    }
-  }, { passive: false });
-
-  document.addEventListener('touchmove', function(e) {
-    e.preventDefault();
-  }, { passive: false });
-
   // canvas
   if (window.innerWidth >= 560) {
     createCanvas(560, 560);
